530d2b436cc371581502716bdefd05e903c0f946
Kabildev <kabildev93@gmail.com>
2020-06-07 16:38:05 +0530

    intial commit with CoreUI template files.

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,17 +1,12 @@
 'use strict'
 
-module.exports = ctx => ({
+module.exports = (ctx) => ({
   map: ctx.file.dirname.includes('examples') ? false : {
     inline: false,
     annotation: true,
     sourcesContent: true
   },
   plugins: {
-    autoprefixer: {
-      cascade: false
-    },
-    // 'cssnano': {},
-    'postcss-combine-duplicated-selectors': {},
-    'postcss-merge-rules': {}
+    autoprefixer: { cascade: false }
   }
 })
